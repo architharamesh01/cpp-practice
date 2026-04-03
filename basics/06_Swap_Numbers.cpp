4f66807c96ac896638ba4d7eaf7533903fc9eeca
architharamesh01 <01.architha@gmail.com>
2026-04-03 19:18:58 +0530

    Create 06_Swap_Numbers.cpp

---
+++ basics/06_Swap_Numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b, temp;
+    cout<<"enter a: ";
+    cin>>a;
+    cout<<"enter b: ";
+    cin>>b;
+    temp = a;
+    a = b;
+    b = temp;
+    cout<<"a = "<< a<<endl<<"b = "<<b;
+    return 0;
+}
