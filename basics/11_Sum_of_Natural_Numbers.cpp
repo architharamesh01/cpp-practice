334bbc3ef65f35c82c714d480eae6f080581bd1d
architharamesh01 <01.architha@gmail.com>
2026-04-03 20:39:28 +0530

    Create 11_Sum_of_Natural_Numbers.cpp

---
+++ basics/11_Sum_of_Natural_Numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int n,i,sum=0;
+    cout<<"enter the number: ";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        sum+=i;
+    }
+    cout<<"sum: "<<sum;
+    return 0;
+}
