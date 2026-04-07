4c49ea2b4292d3f5b8cc6fa5065dd9959f0421c4
architharamesh01 <01.architha@gmail.com>
2026-04-07 22:47:27 +0530

    Create sum_of_arrays.cpp

---
+++ arrays/sum_of_arrays.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i,sum=0;
+    cout<<"enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    for(i=0; i<n; i++){
+        cout<<"enter the number: ";
+        cin>>arr[i];
+    }
+    for(i=0; i<n; i++){
+        sum += arr[i];
+    }
+    cout<<sum;
+    return 0;
+}
