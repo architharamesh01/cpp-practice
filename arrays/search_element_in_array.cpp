985e44fa147b28a607da511608ec9c711c6ef605
architharamesh01 <01.architha@gmail.com>
2026-04-07 22:49:03 +0530

    Create search_element_in_array.cpp

---
+++ arrays/search_element_in_array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i,s,count=0;
+    cout<<"enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    for(i=0; i<n; i++){
+        cout<<"enter the number: ";
+        cin>>arr[i];
+    }
+    cout<<"enetr the element to search: ";
+    cin>>s;
+    for(i=0; i<n; i++){
+        if(arr[i]==s){
+            cout<<i;
+            count++;
+        }
+    }
+    if(count==0){
+        cout<<"no element found";
+    }
+    return 0;
+}
