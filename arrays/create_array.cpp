0372ec4ded19aab404803aa24e3c5b98ccee6fdb
architharamesh01 <01.architha@gmail.com>
2026-04-07 22:46:44 +0530

    Create create_array.cpp

---
+++ arrays/create_array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i;
+    cout<<"enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    for(i=0; i<n; i++){
+        cout<<"enter the number: ";
+        cin>>arr[i];
+    }
+    cout<<"the array: ";
+    for(i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
